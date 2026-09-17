b2a5db12ed5044f33b810c7119e8d7bf5dbcdd2c
schrodinger257 <bm77371043@gmail.com>
2026-09-17 22:22:37 +0300

    feat(extension.ts): read and execute lsp server

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,18 @@ import * as vscode from 'vscode';
 import { MatchBinary } from './utils/match_binary';
 import { DownloadBinary } from './utils/download_binary';
 import { ExtractLSPArchive, LoacateLSPBinary } from './utils/local_binary_actions';
+import path from 'path';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from 'vscode-languageclient/node';
 
+let client: LanguageClient | undefined;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	let id: string | undefined = vscode.window.activeTextEditor?.document.languageId;
 	let allowedIds: string[] = ["python", "javascript", "typescript", "c", "rust", "cpp"];
 	let isBinaryLocated: boolean = false;
 	// get system lsp binary pattern
@@ -23,15 +30,38 @@ export async function activate(context: vscode.ExtensionContext) {
 		const downloadPath: string = await DownloadBinary(context, pattern);
 		await ExtractLSPArchive(downloadPath);
 	}
+	const lspBinary: string = path.join(context.globalStorageUri.fsPath, 'file-downloader-downloads', 'meta-call-lsp');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.window.onDidChangeActiveTextEditor(editor => {
-	});
+	const lspOptions: ServerOptions = {
+		run: {
+			command: lspBinary,
+			transport: TransportKind.stdio,
+		},
+		debug: {
+			command: lspBinary,
+			transport: TransportKind.stdio,
+		},
+	};
 
-	context.subscriptions.push(disposable);
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: allowedIds.map((id) => {return {scheme: 'file', language: id};}),
+		synchronize: {
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	client = new LanguageClient(
+		'MetaCall-lsp',
+		'lsp server for MetaCall',
+		lspOptions,
+		clientOptions
+	);
+
+	await client.start();
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export async function deactivate() {
+	await client?.dispose();
+	client = undefined;
+}
