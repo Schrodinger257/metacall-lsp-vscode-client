cfdc7c230c15d75c7fc7f6a9d9e408a7288cb752
schrodinger257 <bm77371043@gmail.com>
2026-09-17 20:32:23 +0300

    feat(local_binary_actions): implement locating binary and archive extraction logic

--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,27 @@
 import * as vscode from 'vscode';
 import { MatchBinary } from './utils/match_binary';
 import { DownloadBinary } from './utils/download_binary';
+import { ExtractLSPArchive, LoacateLSPBinary } from './utils/local_binary_actions';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
 	let id: string | undefined = vscode.window.activeTextEditor?.document.languageId;
 	let allowedIds: string[] = ["python", "javascript", "typescript", "c", "rust", "cpp"];
+	let isBinaryLocated: boolean = false;
 	// get system lsp binary pattern
 	const pattern: string | undefined = await MatchBinary();
 	if (pattern) {
-		await DownloadBinary(context, pattern);
+		// determine is binary downloaded or not
+		isBinaryLocated = await LoacateLSPBinary(context, pattern);
+
 	}
 	
+	if (!isBinaryLocated && pattern) {
+		const downloadPath: string = await DownloadBinary(context, pattern);
+		await ExtractLSPArchive(downloadPath);
+	}
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
---
+++ src/utils/local_binary_actions.ts
@@ -0,0 +1,68 @@
+import * as path from "path";
+import {ExtensionContext} from "vscode";
+import { readFile } from 'fs/promises';
+import * as vscode from 'vscode';
+import { extract } from 'dir-archiver';
+import * as tar from 'tar';
+
+export async function LoacateLSPBinary(ctx: ExtensionContext, pattern: string) {
+    // path of metacall lsp archive
+    const locatePath: vscode.Uri = vscode.Uri.parse(path.join(ctx.globalStorageUri.fsPath , 'file-downloader-downloads'));
+    let isBinaryLocated: boolean = false;
+    let archiveGot: string = '';
+    // search and locate lsp binary
+    try {
+        const entriesGot = await vscode.workspace.fs.readDirectory(locatePath);
+        
+        const archiveEntriesFound = entriesGot.filter(([name, type]) => {
+            if (type === vscode.FileType.File && (/^meta-call-lsp.*\.zip$/.test(name) || /^meta-call-lsp.*\.tar\.gz$/.test(name))) {
+                archiveGot = name;
+                return true;
+            }
+            return false;
+        });
+
+        if (archiveEntriesFound.length > 0) {
+            const archivePath:string = path.join(ctx.globalStorageUri.fsPath , 'file-downloader-downloads', archiveGot);
+            await ExtractLSPArchive(archivePath);
+            isBinaryLocated = true;
+            return isBinaryLocated;
+        }
+
+        const binaryEntriesFound = entriesGot.filter(([name, type]) =>
+            type === vscode.FileType.File && name === 'meta-call-lsp'
+        );
+
+        if (binaryEntriesFound.length > 0) {
+            isBinaryLocated = true;
+            return isBinaryLocated;
+        }
+
+    } catch(err) {
+        vscode.window.showErrorMessage(`${err}`);
+    }
+
+    return isBinaryLocated;
+}
+
+export async function ExtractLSPArchive(archive: string) {
+    const dest: string = path.dirname(archive);
+
+    try {
+        if (archive.endsWith('.zip')) {
+            await extract(archive, dest);
+            return;
+        }
+
+        if (archive.endsWith('.tar.gz') || archive.endsWith('.tgz')) {
+            await tar.x({
+                file: archive,
+                cwd: dest,
+                strip: 1, // extract contents in the same folder
+            });
+            return;
+        }
+    } catch(err) {
+        vscode.window.showErrorMessage(`${err}`);
+    }
+}
